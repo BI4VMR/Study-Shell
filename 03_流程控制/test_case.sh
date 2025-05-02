3a07ebce8dad40db37d13ec8c86bfbffa6c3adbb
BI4VMR <bi4vmr@outlook.com>
2025-05-02 20:30:38 +0800

    ED23F4C3-EED0-4AD4-9478-A3314B42546A

---
+++ 03_流程控制/test_case.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+x=2
+
+case $x in
+    1)
+        echo "Q1"
+        ;;
+    2)
+        echo "Q2"
+        ;;
+    3)
+        echo "Q3"
+        ;;
+    4)
+        echo "Q4"
+        ;;
+    *)
+        echo "输入值不合法！"
+esac
